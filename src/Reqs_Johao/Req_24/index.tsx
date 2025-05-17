00945e2fcf608b66aa96ec1ecb3a12db5584854c
Johao Andree <johaoagt@gmail.com>
2025-05-17 02:45:31 -0500

    Agrego cambios a mi carpeta de requerimientos, dándole la estructura que considero mejor para la realización de cada requerimiento, además creo archivos base tsx y css para posteriormente empezar a construir las páginas

---
+++ src/Reqs_Johao/Req_24/index.tsx
@@ -0,0 +1,8 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <h1>Página del requerimiento 24</h1>
+  </StrictMode>,
+)
