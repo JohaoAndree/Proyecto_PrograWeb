f569b929bf9aeec996573c517d505c10f184f1df
Johao Andree <johaoagt@gmail.com>
2025-05-17 00:57:49 -0500

    Se actualiza la distribución de carpeta src, agregando carpetas que contengan los requerimientos de cada integrante del grupo

---
+++ src/main.tsx
@@ -0,0 +1,8 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <h1>Página de inicio del proyecto</h1>
+  </StrictMode>,
+)
