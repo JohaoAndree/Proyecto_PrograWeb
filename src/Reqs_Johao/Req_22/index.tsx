74345eb9a56648cbd38899aec4b651cf18e67624
Johao Andree <johaoagt@gmail.com>
2025-05-19 01:00:22 -0500

    Se termina requerimiento 24 creando sus componentes específicos y reutlizando los componentes que se comparten para todos los requerimientos, se instala una librería para importar íconos de react y se hace una pequeña actualización a los index de los requerimientos agregando clases que permitan un mejor diseño responsivo

--- src/Reqs_Johao/Req_22/index.tsx
+++ src/Reqs_Johao/Req_22/index.tsx
@@ -2,16 +2,18 @@ import BarraLateral from '../Shared_Components/BarraLateral'
 import CuerpoPagina from './Components/CuerpoPagina'
 
 const Req22 = () => {
-  return <div className="container-fluid">
-    <div className="row">
-      <div className="col-lg-3 px-0">
-        <BarraLateral />
-      </div>
-      <div className="col-lg-9 px-0">
-        <CuerpoPagina />
+  return (
+    <div className="container-fluid d-flex">
+      <div className="row flex-grow-1">
+        <div className="col-lg-3 px-0">
+          <BarraLateral />
+        </div>
+        <div className="col-lg-9 px-0">
+          <CuerpoPagina />
+        </div>
       </div>
     </div>
-  </div>
+  )
 }
 
 export default Req22
--- src/Reqs_Johao/Req_24/Components/CuerpoPagina.tsx
+++ src/Reqs_Johao/Req_24/Components/CuerpoPagina.tsx
@@ -1,12 +1,39 @@
 import styles from './styles.module.css'
 import Titulo from '../../Shared_Components/Titulo'
+import ListaNoticias, { type Noticia } from './ListaNoticias'
+import Noticia1 from "../../Resources/noticia1.jpg"
+import Noticia2 from "../../Resources/noticia2.jpg"
+import Noticia3 from "../../Resources/noticia3.jpg"
 
 const CuerpoPagina = () => {
     const titulo = "Noticias"
+    const lista : Noticia[] = [
+        {
+            id : 1, 
+            foto : Noticia1, 
+            nombre : "Las últimas novedades sobre el sector del juego en Perú", 
+            descripcion : "Finalmente el país ya cuenta con un robusto e integral marco regulatorio, el cual permitirá que tanto operadores, usuarios y el mismo estado peruano se beneficien de esta actividad de entretenimiento. "
+        },
+        {
+            id : 2, 
+            foto : Noticia2, 
+            nombre : "Algunos no querían, pero los juegos de casino online están contribuyendo a la economía", 
+            descripcion : "Los pronósticos estiman que el mercado de las apuestas online alcanzará un valor aproximado de 315 millones de dólares estadounidenses en 2024."
+        },
+        {
+            id : 3, 
+            foto : Noticia3, 
+            nombre : "Casinos de EEUU ganaron 66.500 millones de dólares en 2023, el mejor año de la industria", 
+            descripcion : "Ni siquiera los gastos por las compras prenavideñas desalentaron a los apostadores: Los casinos ganaron 6.200 millones de dólares en diciembre"
+        }
+    ]
 
     return (
         <div className={styles.CuerpoPagina}>
             <Titulo texto={titulo}/>
+            <div className={styles.ContenedorListaNoticias}>
+                <ListaNoticias data={lista}/>
+            </div>
         </div>
     )
 }
