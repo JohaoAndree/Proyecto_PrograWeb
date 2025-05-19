a3e7766baa13fa3df58e62dd5d59d21d967b8588
D. M. <dmarzal@outlook.com>
2025-05-19 12:04:41 -0500

    Req2

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Req2 from "./Reqs_Diego/Req_2"
+import Req9 from "./Reqs_Diego/Req_9"
+import Req10 from "./Reqs_Diego/Req_10"
+import Req12 from "./Reqs_Diego/Req_12"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <h1>Página de inicio del proyecto</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<h1>Página de inicio del proyecto</h1>} />
+        <Route path="/req2" element={<Req2 />} />
+        <Route path="/req9" element={<Req9 />} />
+        <Route path="/req10" element={<Req10 />} />
+        <Route path="/req12" element={<Req12 />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
