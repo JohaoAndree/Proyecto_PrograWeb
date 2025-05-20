3b9c4afc3f4c3db057f11515c7430af41f878c24
Johao Andree <johaoagt@gmail.com>
2025-05-19 23:34:39 -0500

    Se actualiza etiquetas en el componente ListaOpciones para que cada que se seleccione una opción no se recargue la página sino rediriga directamente

--- src/Reqs_Johao/Shared_Components/ListaOpciones.tsx
+++ src/Reqs_Johao/Shared_Components/ListaOpciones.tsx
@@ -1,12 +1,13 @@
+import { Link } from 'react-router-dom'
 import styles from './SharedComponents.module.css'
 
 const ListaOpciones = () => {
     return (
         <div className={"list-group d-flex flex-column " + styles.ListaOpciones}>
-            <a href="/req22" className={"list-group-item list-group-item-action " + styles.ListItem}>Usuarios</a>
+            <Link to="/req22" className={"list-group-item list-group-item-action " + styles.ListItem}>Usuarios</Link>
             <button className={"list-group-item list-group-item-action " + styles.ListItem} aria-disabled="true">Juegos</button>
-            <a href="/req24" className={"list-group-item list-group-item-action " + styles.ListItem}>Noticias</a>
-            <a href="/req21" className={"list-group-item list-group-item-action " + styles.ListItem}>Estadísticas</a>
+            <Link to="/req24" className={"list-group-item list-group-item-action " + styles.ListItem}>Noticias</Link>
+            <Link to="/req21" className={"list-group-item list-group-item-action " + styles.ListItem}>Estadísticas</Link>
             <button className={"list-group-item list-group-item-action " + styles.ListItem + " " + styles.ItemFinal} aria-disabled="true">Cerrar sesión</button>
         </div>
     )
